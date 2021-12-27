7b266702da0aafcd5e2ebe3d589a03cf51f8c2f2
MikeNotu <mikebateman98@gmail.com>
2021-12-27 13:07:45 -0600

    Next.js + Contentful Tutorial #03

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
-export default function Recipes() {
-  return (
-    <div className="recipe-list">
-      Recipe List
-    </div>
-  )
+import { createClient } from "contentful";
+
+export async function getStaticProps() {
+    const client = createClient({
+        space: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+    });
+
+    const res = await client.getEntries({ content_type: "recipe" });
+
+    return {
+        props: {
+            recipies: res.items,
+        },
+    };
+}
+
+export default function Recipes({ recipies }) {
+    console.log(recipies);
+    return <div className="recipe-list">Recipe List</div>;
 }
