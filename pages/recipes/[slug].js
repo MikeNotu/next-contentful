8755047bdbed1c0fe514c2ce4f4dde53d7278d03
MikeNotu <mikebateman98@gmail.com>
2021-12-27 17:13:35 -0600

    Next.js + Contentful Tutorial #11

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from "contentful";
 import Image from "next/image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Skeleton from "../../components/Skeleton";
 
 const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
@@ -20,7 +21,7 @@ export const getStaticPaths = async () => {
 
     return {
         paths,
-        fallback: false,
+        fallback: true,
     };
 };
 
@@ -37,6 +38,7 @@ export const getStaticProps = async ({ params }) => {
 };
 
 export default function RecipeDetails({ recipe }) {
+    if (!recipe) return <Skeleton />;
     console.log(recipe);
     const { featuredImage, title, cookingTime, ingredients, method } =
         recipe.fields;
