cac447348f445e91d500503ac41fd8db86aab4bc
MikeNotu <mikebateman98@gmail.com>
2021-12-28 12:29:58 -0600

    Next.js + Contentful Tutorial #12

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -31,6 +31,15 @@ export const getStaticProps = async ({ params }) => {
         "fields.slug": params.slug,
     });
 
+    if (!items.length) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        };
+    }
+
     return {
         props: { recipe: items[0] },
         revalidate: 1,
